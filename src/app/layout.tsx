d74bda5c5dc84dbb67a41577acb4bac7c01d0d43
Kev <kevinheyland@icloud.com>
2024-02-24 22:49:19 +0100

    Add logo and update layout description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
     title: 'Kevin Heyland',
-    description: 'Whitness the beauty of Next.js and Tailwind CSS',
+    description: 'Whitness the simplicity of Next.js and Tailwind CSS',
 }
 
 export default function RootLayout({
@@ -16,7 +13,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body>{children}</body>
         </html>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,47 @@
+import Image from 'next/image'
+import Logo from '../../public/HY_LOGO218.png'
+
 export default function Home() {
     return (
-        <main className="center flex min-h-screen flex-col items-center lg:p-24 p-8">
-            <div className="z-10 grid w-full max-w-5xl grid-flow-row items-center justify-center gap-8 font-mono">
-                <div>
-                    <h2 className="text-sm text-lime-400">Code, Creativity, & Change: Navigating the Digital Frontier</h2>
-                    <p>
-                        With a solid foundation in Agile methodology through my certification with the Scrum Alliance, I blend versatile professional experience with specialized
-                        expertise in Agile project management and software development. Starting in retail, where I underwent a remarkable transition from a temporary worker to a
-                        Marketing Manager, my path led me through various strategic positions in the fields of digital marketing and entrepreneurship. As a Senior Digital Marketing
-                        Engineer, I directed significant digital transformation projects at a leading technology company. I demonstrated my entrepreneurial skills as the founder of
-                        an online marketing agency and as the CMO of a startup, where we created groundbreaking visual 3D content.
-                    </p>
-                    <p>
-                        As a Senior UX Designer, I laid the groundwork for top-notch user experiences, before, as a certified Scrum Product Owner and Team Lead, using agile
-                        practices to forge effective backlogs that deliver valuable product increments for clients ranging from mid-sized businesses to large corporations across
-                        various industries. In my current role as Head of Operations, I integrate my passion for artificial intelligence, sustainable development practices, and
-                        diversity into our operational processes and consulting services. Additionally, I shared my extensive knowledge in the area of agile software development as
-                        a co-host of a specialist podcast.
-                    </p>
-                    <p>
-                        This combination of practical professional experience and continuous education allows me to blend operational efficiency with technical innovation,
-                        consistently delivering high-quality results. My career path reflects my relentless drive to evolve, expand my expertise, and implement innovative solutions
-                        in a rapidly changing digital world.
-                    </p>
-                </div>
-                <div>
-                    <h2 className="text-sm text-lime-400">Code, Communication & Compassion: A Commitment to Community</h2>
-                    <p>
-                        Beyond my professional accomplishments, {"I've"} dedicated a portion of my time to voluntary work, underscoring my commitment to contributing positively to
-                        the wider community. This engagement has not only allowed me to give back but has also enriched my own perspective, improving my capabilities in leadership,
-                        empathy, and problem-solving. Whether {"it's"} through leveraging my expertise in technology to support non-profit organizations or mentoring the next
-                        generation of digital professionals, my voluntary work remains a vital aspect of my personal and professional identity, driving me to foster a culture of
-                        innovation, inclusiveness, and social responsibility. digital professionals, my voluntary work remains a vital aspect of my personal and professional
-                        identity, driving me to foster a culture of innovation, inclusiveness, and social responsibility.
-                    </p>
+        <main className="flex justify-center min-h-screen w-full flex-col items-center">
+            <nav className="flex w-full justify-between">
+                <Image src={Logo} alt="logo" width={100} height={100} />
+                <div className="flex items-center px-4"></div>
+            </nav>
+            <div className="p-8 lg:p-24">
+                <div className="z-10 grid w-full max-w-5xl grid-flow-row items-center justify-center gap-8 font-mono">
+                    <div>
+                        <h2 className="text-sm text-lime-400">Code, Creativity, & Change: Navigating the Digital Frontier</h2>
+                        <p>
+                            With a solid foundation in Agile methodology through my certification with the Scrum Alliance, I blend versatile professional experience with
+                            specialized expertise in Agile project management and software development. Starting in retail, where I underwent a remarkable transition from a
+                            temporary worker to a Marketing Manager, my path led me through various strategic positions in the fields of digital marketing and entrepreneurship. As
+                            a Senior Digital Marketing Engineer, I directed significant digital transformation projects at a leading technology company. I demonstrated my
+                            entrepreneurial skills as the founder of an online marketing agency and as the CMO of a startup, where we created groundbreaking visual 3D content.
+                        </p>
+                        <p>
+                            As a Senior UX Designer, I laid the groundwork for top-notch user experiences, before, as a certified Scrum Product Owner and Team Lead, using agile
+                            practices to forge effective backlogs that deliver valuable product increments for clients ranging from mid-sized businesses to large corporations
+                            across various industries. In my current role as Head of Operations, I integrate my passion for artificial intelligence, sustainable development
+                            practices, and diversity into our operational processes and consulting services. Additionally, I shared my extensive knowledge in the area of agile
+                            software development as a co-host of a specialist podcast.
+                        </p>
+                        <p>
+                            This combination of practical professional experience and continuous education allows me to blend operational efficiency with technical innovation,
+                            consistently delivering high-quality results. My career path reflects my relentless drive to evolve, expand my expertise, and implement innovative
+                            solutions in a rapidly changing digital world.
+                        </p>
+                    </div>
+                    <div>
+                        <h2 className="text-sm text-lime-400">Code, Communication & Compassion: A Commitment to Community</h2>
+                        <p>
+                            Beyond my professional accomplishments, {"I've"} dedicated a portion of my time to voluntary work, underscoring my commitment to contributing positively
+                            to the wider community. This engagement has not only allowed me to give back but has also enriched my own perspective, improving my capabilities in
+                            leadership, empathy, and problem-solving. Whether {"it's"} through leveraging my expertise in technology to support non-profit organizations or
+                            mentoring the next generation of digital professionals, my voluntary work remains a vital aspect of my personal and professional identity, driving me to
+                            foster a culture of innovation, inclusiveness, and social responsibility.
+                        </p>
+                    </div>
                 </div>
             </div>
         </main>
