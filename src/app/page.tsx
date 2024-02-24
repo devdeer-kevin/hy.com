1af9709ddbb41478e539129d724ab9910d19d947
Kev <kevinheyland@icloud.com>
2024-02-24 14:42:23 +0100

    Update the whole project and content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,41 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import logoImage from '../../public/logo_squared_240_trans-1.png'
-
 export default function Home() {
     return (
-        <div className="flex h-screen flex-col bg-slate-900">
-            <main className="mx-auto my-auto flex flex-col">
-                <div className="mx-auto mb-4 flex">
-                    <Image src={logoImage} alt="Logo DEVDEER" width={120} height={120} />
+        <main className="center flex min-h-screen flex-col items-center p-24">
+            <div className="z-10 grid w-full max-w-5xl grid-flow-row items-center justify-center gap-8 font-mono">
+                <div>
+                    <h2 className="text-sm text-lime-400">Code, Creativity, & Change: Navigating the Digital Frontier</h2>
+                    <p>
+                        With a solid foundation in Agile methodology through my certification with the Scrum Alliance, I blend versatile professional experience with specialized
+                        expertise in Agile project management and software development. Starting in retail, where I underwent a remarkable transition from a temporary worker to a
+                        Marketing Manager, my path led me through various strategic positions in the fields of digital marketing and entrepreneurship. As a Senior Digital Marketing
+                        Engineer, I directed significant digital transformation projects at a leading technology company. I demonstrated my entrepreneurial skills as the founder of
+                        an online marketing agency and as the CMO of a startup, where we created groundbreaking visual 3D content.
+                    </p>
+                    <p>
+                        As a Senior UX Designer, I laid the groundwork for top-notch user experiences, before, as a certified Scrum Product Owner and Team Lead, using agile
+                        practices to forge effective backlogs that deliver valuable product increments for clients ranging from mid-sized businesses to large corporations across
+                        various industries. In my current role as Head of Operations, I integrate my passion for artificial intelligence, sustainable development practices, and
+                        diversity into our operational processes and consulting services. Additionally, I shared my extensive knowledge in the area of agile software development as
+                        a co-host of a specialist podcast.
+                    </p>
+                    <p>
+                        This combination of practical professional experience and continuous education allows me to blend operational efficiency with technical innovation,
+                        consistently delivering high-quality results. My career path reflects my relentless drive to evolve, expand my expertise, and implement innovative solutions
+                        in a rapidly changing digital world.
+                    </p>
                 </div>
                 <div>
-                    <Link className="my-auto font-mono text-green-200 underline" href="https://devdeer.com/">
-                        Join the devolution.
-                    </Link>
+                    <h2 className="text-sm text-lime-400">Code, Communication & Compassion: A Commitment to Community</h2>
+                    <p>
+                        Beyond my professional accomplishments, {"I've"} dedicated a portion of my time to voluntary work, underscoring my commitment to contributing positively to
+                        the wider community. This engagement has not only allowed me to give back but has also enriched my own perspective, improving my capabilities in leadership,
+                        empathy, and problem-solving. Whether {"it's"} through leveraging my expertise in technology to support non-profit organizations or mentoring the next
+                        generation of digital professionals, my voluntary work remains a vital aspect of my personal and professional identity, driving me to foster a culture of
+                        innovation, inclusiveness, and social responsibility. digital professionals, my voluntary work remains a vital aspect of my personal and professional
+                        identity, driving me to foster a culture of innovation, inclusiveness, and social responsibility.
+                    </p>
                 </div>
-            </main>
-        </div>
+            </div>
+        </main>
     )
 }
