d85eb71385b745d329672931fd28573f4a4871d0
devdeer-kevin <kevin.heyland@devdeer.com>
2024-03-10 22:46:05 +0100

    Refactor loading page to add busy spinner

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,4 +1,6 @@
+import { ArrowPathIcon } from '@heroicons/react/16/solid'
+
 export default function Loading() {
     // Or a custom loading skeleton component
-    return <p>Loading...</p>
+    return <ArrowPathIcon className="h-9 w-9 text-pink-400 animate-spin" />
 }
