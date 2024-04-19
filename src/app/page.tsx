6f3cd9f93db70068a8fa80096719a7aea6b29fef
devdeer-kevin <kevin.heyland@devdeer.com>
2024-04-19 21:06:13 +0200

    Fix typo in import statement in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from 'react'
 import NavigationTopComponent from '../../components/navigationTop'
-import LandingPageComponent from '../../components/langingPage'
+import LandingPageComponent from '../../components/landingPage'
 import FooterComponent from '../../components/footer'
 
 export default function Home(): ReactElement {
