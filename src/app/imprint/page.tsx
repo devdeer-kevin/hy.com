fde91e6d056309dad84c9a4220ee497ff7948632
devdeer-kevin <kevin.heyland@devdeer.com>
2024-05-19 15:50:53 +0200

    feat: Add Imprint page component

---
+++ src/app/imprint/page.tsx
@@ -0,0 +1,16 @@
+export default function Imprint() {
+    return (
+        <>
+            <h1 className="text-slate-600 text-xl font-mono">Impressum</h1>
+            <div className="flex flex-col gap-6 text-slate-600 leading-6 text-center">
+                <p className="text-sm text-slate-600 font-mono">
+                    Kevin Heyland
+                    <br />
+                    Herderstraße 31 <br />
+                    39108 Magdeburg <br />
+                    Deutschland
+                </p>
+            </div>
+        </>
+    )
+}
