607407ccdd89f9874b9cca4d420c8cc71f2b90ab
devdeer-kevin <kevin.heyland@devdeer.com>
2024-05-19 16:15:10 +0200

    feat: Add back button to Imprint page

--- src/app/imprint/page.tsx
+++ src/app/imprint/page.tsx
@@ -1,6 +1,15 @@
+import { ArrowLeftIcon } from '@heroicons/react/16/solid'
+import Link from 'next/link'
+
 export default function Imprint() {
     return (
         <>
+            <div className="absolute top-4 left-4 text-slate-50 w-full">
+                <Link className="flex flex-row gap-2 items-center font-bold text-slate-50 opacity-35" href="/">
+                    <ArrowLeftIcon className="h-5 w-5 text-slate-50" />
+                    Home
+                </Link>
+            </div>
             <h1 className="text-slate-600 text-xl font-mono">Impressum</h1>
             <div className="flex flex-col gap-6 text-slate-600 leading-6 text-center">
                 <p className="text-sm text-slate-600 font-mono">
